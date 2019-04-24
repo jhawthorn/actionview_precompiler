1e8d98794603d3ec23674fcfd90a0d41a3f98f04
John Hawthorn <john@hawthorn.email>
2019-04-24 15:16:05 -0700

    Vaguely working parsing

--- lib/actionview_precompiler/render_parser.rb
+++ lib/actionview_precompiler/render_parser.rb
@@ -1,6 +1,8 @@
 require "parser/current"
 
 module ActionviewPrecompiler
+  RenderCall = Struct.new(:render_type, :template, :locals)
+
   class RenderParser
     def initialize(code)
       @code = code
@@ -8,7 +10,102 @@ module ActionviewPrecompiler
     end
 
     def render_calls
-      []
+      render_nodes = extract_render_nodes(@code)
+      render_nodes.map do |node|
+        parse_render(node)
+      end.compact
+    end
+
+    private
+
+    def parse_render(node)
+      node = node.to_a
+      if (node.length == 3 || node.length == 4) && node[2].type == :str
+        # FIXME: from template vs controller
+        options = {}
+        options[:partial] = node[2]
+        if node.length == 4
+          return unless node[3].type == :hash
+          options[:locals] = node[3]
+        end
+        pp node => options
+        return parse_render_from_options(options)
+      elsif node.length == 3 && node[2].type == :hash
+        options = parse_hash_to_symbols(node[2])
+        return parse_render_from_options(options)
+      else
+        nil
+      end
+    end
+
+    def parse_hash(node)
+      return nil unless node.type == :hash
+
+      node.children.map do |pair|
+        return nil unless pair.type == :pair
+        pair.children
+      end.to_h
+    end
+
+    def parse_hash_to_symbols(node)
+      hash = parse_hash(node)
+      return unless hash
+      pp hash
+      hash.transform_keys do |node|
+        key = parse_sym(node)
+        return unless key
+        key
+      end
+    end
+
+    RENDER_TYPE_KEYS = [:partial, :template, :layout]
+    IGNORED_KEYS = [:formats]
+    ALL_KNOWN_KEYS = [*RENDER_TYPE_KEYS, *IGNORED_KEYS, :locals]
+
+    def parse_render_from_options(options_hash)
+      keys = options_hash.keys
+
+      unless (keys & RENDER_TYPE_KEYS).one?
+        # Must have one of partial:, template:, or layout:
+        return nil
+      end
+
+      unless (keys - ALL_KNOWN_KEYS).empty?
+        # de-opt in case of unknown option
+        return nil
+      end
+
+      render_type = (RENDER_TYPE_KEYS & RENDER_TYPE_KEYS)[0]
+      template = parse_str(options_hash[render_type])
+
+      locals =
+        if options_hash.key?(:locals)
+        else
+          []
+        end
+
+      RenderCall.new(render_type, template)
+    end
+
+    def parse_str(node)
+      node.children[0] if node.type == :str
+    end
+
+    def parse_sym(node)
+      node.children[0] if node.type == :sym
+    end
+
+    def debug(message)
+      warn message
+    end
+
+    def extract_render_nodes(node)
+      return [] unless Parser::AST::Node === node
+      renders = node.children.flat_map { |c| extract_render_nodes(c) }
+      if node.type == :send && node.children[0] == nil && node.children[1] == :render
+        renders << node
+      end
+      renders
     end
   end
 end
--- test/render_parser_test.rb
+++ test/render_parser_test.rb
@@ -3,7 +3,34 @@ require "test_helper"
 module ActionviewPrecompiler
   class RenderParserTest < Minitest::Test
     def test_finds_no_renders
-      assert_equal [], RenderParser.new("1 + 1").render_calls
+      assert_equal [], parse_render_calls("1 + 1")
+    end
+
+    def test_finds_no_renders_from_invalid_calls
+      assert_equal [], parse_render_calls(%q{render()})
+      assert_equal [], parse_render_calls(%q{render(123)})
+      assert_equal [], parse_render_calls(%q{render("foo", 123)})
+      assert_equal [], parse_render_calls(%q{render("foo", {}, {})})
+    end
+
+    def test_finds_simple_render
+      renders = parse_render_calls(%q{render "users/user"})
+      assert_equal 1, renders.length
+      render = renders[0]
+      assert_equal "users/user", render.template
+    end
+
+    def test_finds_simple_render_hash
+      renders = parse_render_calls(%q{render partial: "users/user"})
+      assert_equal 1, renders.length
+      render = renders[0]
+      assert_equal "users/user", render.template
+    end
+
+    private
+
+    def parse_render_calls(code)
+      RenderParser.new(code).render_calls
     end
   end
 end
