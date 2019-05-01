4d1577fa69bf4719bd950b64e677760a3665c893
John Hawthorn <john@hawthorn.email>
2019-05-01 13:12:28 -0700

    Avoid warning in parser test

--- test/render_parser_test.rb
+++ test/render_parser_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 module ActionviewPrecompiler
   class RenderParserTest < Minitest::Test
     def test_finds_no_renders
-      assert_equal [], parse_render_calls("1 + 1")
+      assert_equal [], parse_render_calls("x = x = 1 + 1")
     end
 
     def test_finds_no_renders_from_invalid_calls
