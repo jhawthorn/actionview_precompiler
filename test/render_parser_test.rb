b94baa6b7a22b0742fba85fa5f26c0d9f95ec2e1
John Hawthorn <john@hawthorn.email>
2020-11-04 13:03:25 -0800

    Add test for call with trailing comma

--- test/render_parser_test.rb
+++ test/render_parser_test.rb
@@ -159,6 +159,13 @@ module ActionviewPrecompiler
       assert_equal [:user], renders[0].locals_keys
     end
 
+    def test_finds_renders_with_trailing_comma
+      renders = parse_render_calls(%q{render("discussions/sidebar", discussion: discussion,)})
+      assert_equal 1, renders.length
+      assert_equal "discussions/_sidebar", renders[0].virtual_path
+      assert_equal [:discussion], renders[0].locals_keys
+    end
+
     def test_render_object
       renders = parse_render_calls(%q{render partial: "users/user", object: @user })
       assert_equal 1, renders.length
