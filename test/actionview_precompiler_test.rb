fd81f2321d35e2d3d432b16ce5724426ec06cc73
John Hawthorn <john@hawthorn.email>
2020-11-12 16:54:01 -0800

    Add more assertions to smoke test

--- test/actionview_precompiler_test.rb
+++ test/actionview_precompiler_test.rb
@@ -6,6 +6,24 @@ class ActionviewPrecompilerTest < Minitest::Test
   end
 
   def test_precompile_works
-    ActionviewPrecompiler.precompile
+    reset_action_view!
+
+    compiled_templates = []
+    callback = ->(name, start, finish, id, payload) do
+      compiled_templates << payload[:virtual_path]
+    end
+
+    # We should compile the expected template
+    ActiveSupport::Notifications.subscribed(callback, "!compile_template.action_view") do
+      ActionviewPrecompiler.precompile
+    end
+    assert_includes compiled_templates, "users/_user"
+
+    # There should be nothing to compile on a second run
+    compiled_templates = []
+    ActiveSupport::Notifications.subscribed(callback, "!compile_template.action_view") do
+      ActionviewPrecompiler.precompile
+    end
+    assert_empty compiled_templates
   end
 end
