4fd03ec03a2e52a17e979ff476affdc2ca851ea4
Kevin Newton <kddnewton@gmail.com>
2023-10-08 14:14:44 -0400

    Fix up syntax tree backend

--- lib/actionview_precompiler/ast_parser/syntax_tree.rb
+++ lib/actionview_precompiler/ast_parser/syntax_tree.rb
@@ -57,31 +57,23 @@ module ActionviewPrecompiler
       # will raise an error if the hash doesn't match the format we expect or
       # if the hash contains any splats.
       def to_hash
-        case node
-        in SyntaxTree::HashLiteral[assocs:]
-        in SyntaxTree::BareAssocHash[assocs:]
+        if hash? && node.assocs.all? { |assoc| assoc.is_a?(SyntaxTree::Assoc) }
+          node.assocs.to_h { |assoc| [RenderNode.new(assoc.key), RenderNode.new(assoc.value)] }
         else
           raise CompilationError, "Unexpected node type: #{node.class.name}"
-        end
-
-        assocs.to_h do |assoc|
-          case assoc
-          in SyntaxTree::Assoc[key:, value:]
-            [RenderNode.new(key), RenderNode.new(value)]
-          else
-            raise CompilationError, "Unexpected node type: #{node.class.name}"
-          end
         end
       end
 
       # Converts the node into a string value. Only handles plain string
       # content, and will raise an error if the node contains interpolation.
       def to_string
-        case node
-        in SyntaxTree::StringLiteral[parts: [SyntaxTree::TStringContent[value:]]]
-          value
-        in SyntaxTree::StringLiteral[parts:]
-          raise CompilationError, "Unexpected string parts type: #{parts.inspect}"
+        if string?
+          parts = node.parts
+          if parts.length != 1 || !parts[0].is_a?(SyntaxTree::TStringContent)
+            raise CompilationError, "Unexpected string parts type: #{parts.inspect}"
+          end
+
+          node.parts[0].value
         else
           raise CompilationError, "Unexpected node type: #{node.class.name}"
         end
@@ -90,13 +82,14 @@ module ActionviewPrecompiler
       # Converts the node into a symbol value. Only handles labels and plain
       # symbols, and will raise an error if the node contains interpolation.
       def to_symbol
-        case node
-        in SyntaxTree::Label[value:]
-          value.chomp(":").to_sym
-        in SyntaxTree::SymbolLiteral[value: SyntaxTree::Ident[value:]]
-          value.to_sym
-        in SyntaxTree::SymbolLiteral[value:]
-          raise CompilationError, "Unexpected symbol value type: #{value.inspect}"
+        if node.is_a?(SyntaxTree::Label)
+          node.value.chomp(":").to_sym
+        elsif node.is_a?(SyntaxTree::SymbolLiteral)
+          if !node.value.is_a?(SyntaxTree::Ident)
+            raise CompilationError, "Unexpected symbol value type: #{node.value.inspect}"
+          end
+
+          node.value.to_sym
         else
           raise CompilationError, "Unexpected node type: #{node.class.name}"
         end
@@ -107,7 +100,7 @@ module ActionviewPrecompiler
     # out the render calls. After visiting the tree, the #render_calls method
     # will return the hash expected by the #parse_render_nodes method.
     class RenderVisitor < SyntaxTree::Visitor
-      MESSAGE = /\A(render|render_to_string|layout)\z/
+      MESSAGE = /\A(?:render|render_to_string|layout)\z/
 
       attr_reader :render_calls
 
@@ -116,28 +109,23 @@ module ActionviewPrecompiler
       end
 
       visit_method def visit_command(node)
-        case node
-        in SyntaxTree::Command[
-             message: SyntaxTree::Ident[value: MESSAGE],
-             arguments: SyntaxTree::Args[parts:]
-           ]
-          argument_nodes = parts.map { |part| RenderNode.new(part) }
-          render_calls[$1.to_sym] << RenderCall.new(argument_nodes)
-        else
+        if node.message.is_a?(SyntaxTree::Ident) &&
+           node.message.value.match?(MESSAGE) &&
+           node.arguments.is_a?(SyntaxTree::Args)
+          argument_nodes = node.arguments.parts.map { |part| RenderNode.new(part) }
+          render_calls[node.message.value.to_sym] << RenderCall.new(argument_nodes)
         end
 
         super
       end
 
-      visit_method def visit_fcall(node)
-        case node
-        in SyntaxTree::FCall[
-             value: SyntaxTree::Ident[value: MESSAGE],
-             arguments: SyntaxTree::ArgParen[arguments: SyntaxTree::Args[parts:]]
-           ]
-          argument_nodes = parts.map { |part| RenderNode.new(part) }
-          render_calls[$1.to_sym] << RenderCall.new(argument_nodes)
-        else
+      visit_method def visit_call(node)
+        if node.message.is_a?(SyntaxTree::Ident) &&
+           node.message.value.match?(MESSAGE) &&
+           node.arguments.is_a?(SyntaxTree::ArgParen) &&
+           node.arguments.arguments.is_a?(SyntaxTree::Args)
+          argument_nodes = node.arguments.arguments.parts.map { |part| RenderNode.new(part) }
+          render_calls[node.message.value.to_sym] << RenderCall.new(argument_nodes)
         end
 
         super
